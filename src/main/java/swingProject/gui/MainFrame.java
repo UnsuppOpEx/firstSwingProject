48fe8c18b5a4b374b5bafc2d77b31b63ec9ec67c
Anton <zantn22@gmail.com>
2020-07-19 23:34:09 +0300

    Переработка класса SwingProject

--- src/main/java/swingProject/gui/MainFrame.java
+++ src/main/java/swingProject/gui/MainFrame.java
@@ -3,12 +3,5 @@ package swingProject.gui;
 import javax.swing.*;
 
 public class MainFrame extends JFrame {
-    private JPanel contentPanel;
 
-    public void setContent(JPanel panel) {
-        this.contentPanel = panel;
-    }
-
-    public MainFrame() {
-    }
 }
