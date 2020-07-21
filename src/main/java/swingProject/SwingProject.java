1b2212e05454c97e689b40267fe51ed3b776e687
Anton <zantn22@gmail.com>
2020-07-21 11:12:34 +0300

    Перенос менеджера компановки и кнопок в StartPanel

--- src/main/java/swingProject/SwingProject.java
+++ src/main/java/swingProject/SwingProject.java
@@ -1,25 +1,18 @@
 package swingProject;
 
-import javax.swing.UIManager.*;
-
-import net.miginfocom.swing.MigLayout;
 import swingProject.gui.MainFrame;
 import swingProject.gui.startPanel.StartPanel;
 
 import javax.swing.*;
+import javax.swing.plaf.nimbus.NimbusLookAndFeel;
 import java.awt.*;
 
 public class SwingProject {
     public static void main(String[] args) {
         try {
-            for (LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
+            UIManager.setLookAndFeel(new NimbusLookAndFeel());
         } catch (Exception e) {
-            // If Nimbus is not available, you can set the GUI to another look and feel.
+            e.printStackTrace();
         }
 
         MainFrame mainFrame = new MainFrame();
@@ -29,23 +22,10 @@ public class SwingProject {
         mainFrame.setLayout(new BorderLayout());
         JLabel label = new JLabel("Версия программы 0.0.1");
 
-        JButton jButton1 = new JButton();
-        JButton jButton2 = new JButton();
-        JButton jButton3 = new JButton();
-
-        StartPanel panel = new StartPanel();
-        panel.setLayout(new MigLayout("center"));
-        panel.add(jButton1, "gaptop 150, wrap 20, w 250!, h 50!");
-        panel.add(jButton2, "wrap 20, w 250!, h 50!");
-        panel.add(jButton3, "wrap 20, w 250!, h 50!");
-
-
-        mainFrame.add(panel);
-        mainFrame.add(label,BorderLayout.PAGE_END);
+        mainFrame.add(new StartPanel());
+        mainFrame.add(label,BorderLayout.PAGE_END); // TODO:перенести label в другой угол.
         mainFrame.setResizable(false);
         mainFrame.setLocationRelativeTo(null);
         mainFrame.setVisible(true);
-
-
     }
 }
--- src/main/java/swingProject/gui/startPanel/StartPanel.java
+++ src/main/java/swingProject/gui/startPanel/StartPanel.java
@@ -5,5 +5,14 @@ import net.miginfocom.swing.MigLayout;
 import javax.swing.*;
 
 public class StartPanel extends JPanel {
+    public StartPanel() {
+        JButton jButton1 = new JButton();
+        JButton jButton2 = new JButton();
+        JButton jButton3 = new JButton();
 
+        setLayout(new MigLayout("center"));
+        add(jButton1, "gaptop 150, wrap 20, w 250!, h 50!");
+        add(jButton2, "wrap 20, w 250!, h 50!");
+        add(jButton3, "wrap 20, w 250!, h 50!");
+    }
 }
