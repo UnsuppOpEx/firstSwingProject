e0d4641aec12e5f0dbf205d9ac5c6f45b1d5520f
Anton <zantn22@gmail.com>
2020-07-23 21:12:11 +0300

    Перенос логики в MainFrame

--- src/main/java/swingProject/SwingProject.java
+++ src/main/java/swingProject/SwingProject.java
@@ -16,16 +16,6 @@ public class SwingProject {
         }
 
         MainFrame mainFrame = new MainFrame();
-        mainFrame.setTitle("Меню");
-        mainFrame.setSize(800, 600);
-        mainFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        mainFrame.setLayout(new BorderLayout());
-        JLabel label = new JLabel("Версия программы 0.0.1");
 
-        mainFrame.add(new StartPanel());
-        mainFrame.add(label,BorderLayout.PAGE_END); // TODO:перенести label в другой угол.
-        mainFrame.setResizable(false);
-        mainFrame.setLocationRelativeTo(null);
-        mainFrame.setVisible(true);
     }
 }
