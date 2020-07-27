32f8bc97f669c2094b388b336c34d648b097bbc1
Anton <zantn22@gmail.com>
2020-07-28 01:27:07 +0300

    Работа с классами пакета entities: переменные конструкторы , setters and getters

--- src/main/java/swingProject/entities/computerComponents/Component.java
+++ src/main/java/swingProject/entities/computerComponents/Component.java
@@ -1,4 +1,13 @@
 package swingProject.entities.computerComponents;
 
 public abstract class Component {
+    private String manufacturer;
+    private String model;
+    private int yearRelease;
+
+    public Component(String manufacturer, String model, int yearRelease) {
+        this.manufacturer = manufacturer;
+        this.model = model;
+        this.yearRelease = yearRelease;
+    }
 }
